babcd101fd91df7f63429c17cbd4757b94e9b057
Dripzoid <dripzoidapp@gmail.com>
2025-09-13 01:58:38 +0530

    Update checkUsers.js

--- checkUsers.js
+++ checkUsers.js
@@ -1,6 +1,17 @@
-const sqlite3 = require("sqlite3").verbose();
-const db = new sqlite3.Database("./dripzoid.db");
 
+import dotenv from "dotenv";
+
+dotenv.config();
+const sqlite3 = require("sqlite3").verbose();
+const dbPath = process.env.DATABASE_FILE || path.resolve("./dripzoid.db");
+
+const db = new sqlite3.Database(dbPath, (err) => {
+  if (err) {
+    console.error("❌ Failed to connect to database:", err.message);
+  } else {
+    console.log("✅ Connected to SQLite at:", dbPath);
+  }
+});
 db.all("SELECT * FROM users", [], (err, rows) => {
   if (err) {
     throw err;
@@ -8,3 +19,4 @@ db.all("SELECT * FROM users", [], (err, rows) => {
   console.log(rows);
   db.close();
 });
+
